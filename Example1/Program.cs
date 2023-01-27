7667a2f449bd181a42a17cd18d4ea765c03417b2
Polina <ms.maylin@inbox.ru>
2023-01-27 20:35:04 +0300

    Example1 added

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+void SumPositiveAndNegativeElements(int[] arr, out int sumP, out int sumN)
+{
+    sumP = 0;
+    sumN = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            sumP += arr[i];
+        }
+        else
+        {
+            sumN += arr[i]; // sumNegative = sumNegative + array[i]
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+int[] array = FillArrayWithRandomNumbers(6, -9, 9);
+
+PrintArray(array);
+
+SumPositiveAndNegativeElements(array, out int sumP, out int sumN);
+
+System.Console.WriteLine($"Сумма положительных = {sumP}, отрицательных = {sumN}");
